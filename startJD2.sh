4920216c4507bf1902a8ea856c9860a8306fa7cd
Don Koopa <koopzington@gmail.com>
2017-04-07 11:10:36 +0200

    Run JDownloader as custom user

--- startJD2.sh
+++ startJD2.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
+USER="jdownloader"
+
+echo "JDownloader settings"
+echo "===================="
+echo
+echo "User:       ${USER}"
+echo "UID:        ${JDOWNLOADER_UID:=666}"
+echo "GID:        ${JDOWNLOADER_GID:=666}"
+echo
+
 function stopJD2 {
+printf "Stopping JDownloader"
 	PID=$(cat JDownloader.pid)
 	kill $PID
 	wait $PID
+	echo  "[DONE]"
 	exit
 }
 
 trap stopJD2 EXIT
 
-java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar &
+printf "Updating UID / GID... "
+[[ $(id -u ${USER}) == ${JDOWNLOADER_UID} ]] || usermod  -o -u ${JDOWNLOADER_UID} ${USER}
+[[ $(id -g ${USER}) == ${JDOWNLOADER_GID} ]] || groupmod -o -g ${JDOWNLOADER_GID} ${USER}
+echo "[DONE]"
+
+printf "Setting permissions... "
+chown -R ${USER}: /opt/JDownloader
+chown ${USER}: /media
+echo "[DONE]"
+
+echo "Starting JDownloader..."
+exec su -pc "java -Djava.awt.headless=true -jar /opt/JDownloader/JDownloader.jar 2>&1 >/dev/null" ${USER}
 
 while true; do
 	sleep inf
 done
-
